4071fd7d9a5c09483bc9c423da637659c094f2fb
brilliantyy <yujia6366@163.com>
2016-04-29 21:30:13 +0800

    Update

---
+++ public/javascripts/index.js
@@ -0,0 +1,23 @@
+var video_list = ["/videos/Beach-Camera.mp4",
+    "/videos/Up.mp4",
+    "/videos/The-Crosswalk.mp4"
+];
+var video_index     = 0;
+var video_player    = null;
+
+function onload(){
+    video_player = document.getElementById("idle_video");
+    video_player.setAttribute("src", video_list[video_index]);
+    video_player.play();
+}
+
+function onVideoEnded(){
+    if(video_index < video_list.length - 1){
+        video_index++;
+    }
+    else{
+        video_index = 0;
+    }
+    video_player.setAttribute("src", video_list[video_index]);
+    video_player.play();
+}
